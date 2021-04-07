482b42c222b8db2ce9a85616e7089348490dfd79
ayushsinghal12 <81246687+ayushsinghal12@users.noreply.github.com>
2021-04-07 19:32:50 +0530

    Add files via upload

---
+++ FORM/app.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA4flzYZ4xLupCjJ-YpnDq6geuEMVHGH40",
+    authDomain: "corona-testing-507e8.firebaseapp.com",
+    databaseURL: "https://corona-testing-507e8-default-rtdb.firebaseio.com",
+    projectId: "corona-testing-507e8",
+    storageBucket: "corona-testing-507e8.appspot.com",
+    messagingSenderId: "464923671354",
+    appId: "1:464923671354:web:4ecbf888c9c0cff9186e03"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
